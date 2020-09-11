263cf374fc36010a8fb050ae31d9cdbe88121250
alfonsoruiz <1800ruiz@gmail.com>
2020-09-10 20:57:34 -0700

    word count application

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,8 @@
 # Your code here
+import math
+import random
+
+lookup = {}
 
 
 def slowfun_too_slow(x, y):
@@ -9,17 +13,16 @@ def slowfun_too_slow(x, y):
 
     return v
 
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
-
+    pass
 
 
 # Do not modify below this line!
-
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,25 @@
 def word_count(s):
-    # Your code here
+    word_count = dict()
+    words = s.split()
+    ignore_chars = '":;,.-+=/\\|[]{}()*^&'
 
+    for word in words:
+        word = word.lower().strip(ignore_chars)
+
+        if word == '':
+            continue
+
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+
+    return word_count
 
 
 if __name__ == "__main__":
-    print(word_count(""))
+    print(word_count(''))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
